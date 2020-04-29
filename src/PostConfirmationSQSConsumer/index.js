1573ab01f4e244e04bc49d3483060d2c85bedc7e
Andrew971 <sordier.andrew@gmail.com>
2020-04-29 03:17:36 -0700

    deploy new lambda version for cognito trigger

--- src/PostConfirmationSQSConsumer/index.js
+++ src/PostConfirmationSQSConsumer/index.js
@@ -5,47 +5,47 @@ const dynamodb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
 
 exports.handler = async (event, context, callback) => {
   try {
-    // for (record of event.Records) {
-    //   console.log('record', record)
-    //   const body = JSON.parse(record.body)
-    //   const Message = JSON.parse(body.Message)
-    //   console.log('Message', Message)
-    const {
-      userName,
-      userPoolId,
-      region,
-      request,
-    } = event
+    for (record of event.Records) {
+      console.log('record', record)
+      const body = JSON.parse(record.body)
+      const Message = JSON.parse(body.Message)
+      console.log('Message', Message)
+      const {
+        userName,
+        userPoolId,
+        region,
+        request,
+      } = event
 
-    const {
-      userAttributes = null,
-    } = request
+      const {
+        userAttributes = null,
+      } = request
 
-    const timestamp = Math.floor(Date.now() / 1000)
-    console.log('request timestamp', timestamp)
+      const timestamp = Math.floor(Date.now() / 1000)
+      console.log('request timestamp', timestamp)
 
 
-    const updateProfile = {
-      TableName: process.env.DYNAMODB_TABLE,
-      Item: {
-        UserId: userAttributes.sub,
-        Type: 'Profile',
-        Email: userAttributes.email,
-        Extra_Field: {
-          userName,
-          userPoolId,
-          region
-        },
-        Created_At: timestamp,
-        Updated_At: timestamp,
+      const updateProfile = {
+        TableName: process.env.DYNAMODB_TABLE,
+        Item: {
+          UserId: userAttributes.sub,
+          Type: 'Profile',
+          Email: userAttributes.email,
+          Extra_Field: {
+            userName,
+            userPoolId,
+            region
+          },
+          Created_At: timestamp,
+          Updated_At: timestamp,
+        }
       }
+      console.log('request data', updateProfile)
+
+      const putJobRequest = await dynamodb.put(updateProfile).promise()
+
+      return `Successfully processed ${event.Records.length} records.`
     }
-    console.log('request data', updateProfile)
-
-    await dynamodb.put(updateProfile).promise()
-
-    return `Successfully processed ${event.Records.length} records.`
-    // }
   } catch (error) {
     console.log({
       message: 'A problem ocurred',
